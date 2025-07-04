46f399d0f44d7a786065e8499e0aa9160738fe3c
Anderson Vieira <asvieira.dev@gmail.com>
2025-07-04 11:50:42 -0300

    test: add unit test for CNPJ value object to validate if CNPJ is invalid

---
+++ src/modules/company/domain/models/__tests__/cnpj.spec.ts
@@ -0,0 +1,19 @@
+import { InvalidDocumentException } from '../../exceptions/invalid-document.exception'
+import { CNPJValidator } from '../../validator/cnpj.validator'
+import { CNPJ } from '../cnpj'
+
+describe('CNPJ', () => {
+  it('should throw InvalidDocumentException for invalid CNPJ', () => {
+    const invalidCNPJ = '12345678910111'
+
+    const validateMock = jest.fn().mockReturnValue(false)
+    const mockValidator: CNPJValidator = {
+      validate: validateMock,
+    }
+
+    expect(() => CNPJ.create(invalidCNPJ, mockValidator)).toThrow(
+      InvalidDocumentException,
+    )
+    expect(validateMock).toHaveBeenCalledWith(invalidCNPJ)
+  })
+})
